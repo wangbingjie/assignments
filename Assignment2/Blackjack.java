64b4da27bd81d9ee01d62e94c94066929facf9b5
Bingjie Wang <b.wang@pitt.edu>
2016-02-22 15:01:17 -0500

    fixed rules

--- Assignment2/Blackjack.java
+++ Assignment2/Blackjack.java
@@ -71,6 +71,7 @@ public class Blackjack {
         objPlayer._hand ++;
         int numAces = 0, numAcesDealer = 0;
         int points = 0, pointsDealer = 0;
+        int scoreDealer = 0;
 
         System.out.println("");
         System.out.print("Enter amount to bet > ");
@@ -134,66 +135,15 @@ public class Blackjack {
           blackjack = true;
         }
 
-        // Dealer's first deal
-        r = rng.nextInt(13); // 0-12
-        r += 1; // 1-13 -> value of the card
-        Card cDealer = new Card(r);
-
-        // get number of Aces
-        if (r == 1) {
-          numAcesDealer += 1;
-        }
-
-        r2 = rng.nextInt(13); // 0-12
-        r2 += 1; // 1-13 -> value of the card
-        Card cDealer2 = new Card(r2);
-        //System.out.println(c2.getPoint());
-        //System.out.println(" " + cDealer2.toString());
-
-        // get number of Aces
-        if (r2 == 1) {
-          numAcesDealer += 1;
-        }
-        pointsDealer = cDealer.getPoint()+cDealer2.getPoint();
-        int scoreDealer = classScore.getScore(pointsDealer, numAcesDealer);
-
-        // The house player will follow a simple set of rules:
-        // If the score is greater than 21, the house loses
-        // Otherwise, if the score is greater than or equal to 18 but
-        // less than or equal to 21, the house will stay
-        // If the score is exactly 17, and at least one card is an ace
-        // (a "soft" 17), the house will hit.
-        // If no cards are aces (a "hard" 17), the house will stay.
-        // Otherwise, the house will hit
-
-        boolean houseStay = false;
-        if (scoreDealer > 21) {
-          houseStay = true;
-          System.out.println("Dealer busted!");
-        } else if (scoreDealer >= 18 && scoreDealer <=21 ) {
-          houseStay = true;
-        } else if (scoreDealer == 17 && numAcesDealer >= 1) {
-          houseStay = false;
-        } else if (scoreDealer == 17 && numAcesDealer == 0) {
-          houseStay = true;
-        } else {
-          houseStay = false;
-        }
-
-        boolean keepDeal = true;
-        while (keepDeal) {
+        System.out.print("[H]it or [S]tay? > ");
+        while (!sc.hasNext("H") && !sc.hasNext("h") && !sc.hasNext("Hit") && !sc.hasNext("hit")
+        && !sc.hasNext("S") && !sc.hasNext("s") && !sc.hasNext("Stay") && !sc.hasNext("stay")) {
           System.out.print("[H]it or [S]tay? > ");
-          while (!sc.hasNext("H") && !sc.hasNext("h")
-          && !sc.hasNext("Hit") && !sc.hasNext("hit")
-          && !sc.hasNext("S") && !sc.hasNext("s")
-          && !sc.hasNext("Stay") && !sc.hasNext("stay")) {
-            System.out.print("[H]it or [S]tay? > ");
-            sc.next();
-          }
-          String action = sc.next();
-          // "hit" - accept another card
-          if (action.equalsIgnoreCase("H") || action.equalsIgnoreCase("Hit")) {
-            keepDeal = true;
+          sc.next();
+        }
+        String action = sc.next();
+
+        while ( action.equalsIgnoreCase("H") || action.equalsIgnoreCase("Hit") ) {
             int r3 = rng.nextInt(13); // 0-12
             r3 += 1; // 1-13 -> value of the card
             Card cHit = new Card(r3);
@@ -202,105 +152,148 @@ public class Blackjack {
             }
             points += cHit.getPoint();
             score = classScore.getScore(points, numAces);
+
             System.out.println("Card dealt: " + cHit.toString());
             System.out.println("Score: " + score);
 
-            if (score > 21) {
-              keepDeal = false;
-            } else {
-              keepDeal = true;
+            if (score <= 21) {
+
+            System.out.print("[H]it or [S]tay? > ");
+            action = sc.next();
+          } else {
+            action = "s";
+          }
+        }
+
+          // Now player stays
+          // dealer turns cards
+          System.out.println("DEALER DEAL");
+          r = rng.nextInt(13); // 0-12
+          r += 1; // 1-13 -> value of the card
+          Card cDealer = new Card(r);
+
+          // get number of Aces
+          if (r == 1) {
+            numAcesDealer += 1;
+          }
+
+          r2 = rng.nextInt(13); // 0-12
+          r2 += 1; // 1-13 -> value of the card
+          Card cDealer2 = new Card(r2);
+          System.out.print(cDealer.toString());
+          System.out.println(" " + cDealer2.toString());
+
+          // get number of Aces
+          if (r2 == 1) {
+            numAcesDealer += 1;
+          }
+          pointsDealer = cDealer.getPoint()+cDealer2.getPoint();
+          scoreDealer = classScore.getScore(pointsDealer, numAcesDealer);
+          System.out.println("Score: " + scoreDealer);
+
+          // The house player will follow a simple set of rules:
+          // If the score is greater than 21, the house loses
+          // Otherwise, if the score is greater than or equal to 18 but
+          // less than or equal to 21, the house will stay
+          // If the score is exactly 17, and at least one card is an ace
+          // (a "soft" 17), the house will hit.
+          // If no cards are aces (a "hard" 17), the house will stay.
+          // Otherwise, the house will hit
+
+          boolean houseStay = false;
+          if (scoreDealer > 21) {
+            houseStay = true;
+          } else if (scoreDealer >= 18 && scoreDealer <= 21 ) {
+            houseStay = true;
+          } else if (scoreDealer == 17 && numAcesDealer >= 1) {
+            houseStay = false;
+          } else if (scoreDealer == 17 && numAcesDealer == 0) {
+            houseStay = true;
+          } else {
+            houseStay = false;
+          }
+
+          while (houseStay == false) {
+            System.out.println("Hit!");
+            int r4 = rng.nextInt(13); // 0-12
+            r4 += 1; // 1-13 -> value of the card
+            Card cDealerHit = new Card(r4);
+            if (r4 == 1) {
+              numAcesDealer += 1;
             }
+            pointsDealer += cDealerHit.getPoint();
+            scoreDealer = classScore.getScore(pointsDealer, numAcesDealer);
+            System.out.println("Card dealt: " + cDealerHit.toString());
 
-            // Dealer deal
-            if (houseStay == false && score <= 21) {
-              int r4 = rng.nextInt(13); // 0-12
-              r4 += 1; // 1-13 -> value of the card
-              Card cDealerHit = new Card(r4);
-
-              if (r4 == 1) {
-                numAcesDealer += 1;
-              }
-
-              pointsDealer += cDealerHit.getPoint();
-              scoreDealer = classScore.getScore(pointsDealer, numAcesDealer);
-
-              if (score <= 21 && scoreDealer > 21) {
-                keepDeal = false;
-                houseStay = true;
-              } else if (score <= 21 && scoreDealer >= 18 && scoreDealer <= 21 ) {
-                houseStay = true;
-              } else if ( score <= 21 && scoreDealer == 17 && numAcesDealer >= 1) {
-                houseStay = false;
-                System.out.println("Hit!");
-              } else if (score <= 21 && scoreDealer == 17 && numAcesDealer == 0) {
-                houseStay = true;
-              } else if (score <= 21){
-                houseStay = false;
-                System.out.println("Hit!");
-              }
-            } else {// if (houseStay == false) loop
-              if (score < 21) {
-              System.out.println("Stay!");
+            if (score <= 21 && scoreDealer > 21) {
+              houseStay = true;
+            } else if (scoreDealer >= 18 && scoreDealer <= 21 ) {
+              houseStay = true;
+            } else if (scoreDealer == 17 && numAcesDealer >= 1) {
+              houseStay = false;
+            } else if (scoreDealer == 17 && numAcesDealer == 0) {
+              houseStay = true;
+            } else {
+              houseStay = true;
             }
           }
 
-        } else {
-          // player stay
-          keepDeal = false;
-          // Once the player stays, the dealer will turn over their cards.
-          System.out.println("Dealer score: " + scoreDealer);
-        }
-      } // while (keepDeal) loop
-
-      // Now get the game results
-      // At the end of each hand, the amount of money won or lost,
-      // along with the result, should be displayed
-      if (blackjack == true) {
-        // The player has a Blackjack (exactly 21 points from only two cards)
-        // The player receives all of their money back, plus 1.5 times their bet.
-        System.out.println("The player has a blackjack!");
-        objPlayer._money = objPlayer._money + betAmount * 1.5;
-        System.out.println("You won $" + (betAmount*1.5) + "!");
-      } else if (score == scoreDealer) {
-        // the player does not lose or gain any money
-        System.out.println("Push!");
-      } else if (score > scoreDealer && score <= 21 ) {
-        // The player receives all of their money back,
-        // plus wins that exact same amount they bet.
-        objPlayer._money = objPlayer._money + betAmount;
-        System.out.println("Player won!");
-        System.out.println("You won $" + betAmount + "!");
-      } else if (score < scoreDealer && scoreDealer <= 21 ){
-        // score < scoreDealer
-        objPlayer._money = objPlayer._money - betAmount;
-        System.out.println("Dealer won!");
-        System.out.println("You lost $" + betAmount + "!");
-      } else if (scoreDealer > 21) {
-        System.out.println("Dealer busted!");
-        System.out.println("Player won!");
-        System.out.println("You won $" + betAmount + "!");
-      } else if (score > 21) {
-        System.out.println("Player busted!");
-        objPlayer._money = objPlayer._money - betAmount;
-        System.out.println("Dealer won!");
-        System.out.println("You lost $" + betAmount + "!");
-      }
-      endGame = false;
-
-        if (objPlayer._money == 0) {
-          System.out.println("You have no more money to bet!");
-          endGame = true;
-          objPlayer.writeInfo(name);
-          System.out.println("Thank you for playing Infinite Blackjack!");
-        }
-      } // choice = y
+          // Now get the game results
+          // At the end of each hand, the amount of money won or lost,
+          // along with the result, should be displayed
+          if (blackjack == true) {
+            // The player has a Blackjack (exactly 21 points from only two cards)
+            // The player receives all of their money back, plus 1.5 times their bet.
+            System.out.println("The player has a blackjack!");
+            objPlayer._money = objPlayer._money + betAmount * 1.5;
+            System.out.println("You won $" + (betAmount*1.5) + "!");
+          } else if (score == scoreDealer) {
+            // the player does not lose or gain any money
+            System.out.println("Player score: " + score);
+            System.out.println("Delaer score: " + scoreDealer);
+            System.out.println("Push!");
+          } else if (score > scoreDealer && score <= 21 ) {
+            // The player receives all of their money back,
+            // plus wins that exact same amount they bet.
+            objPlayer._money = objPlayer._money + betAmount;
+            System.out.println("Player score: " + score);
+            System.out.println("Delaer score: " + scoreDealer);
+            System.out.println("Player won!");
+            System.out.println("You won $" + betAmount + "!");
+          } else if (score < scoreDealer && scoreDealer <= 21 ){
+            System.out.println("Player score: " + score);
+            System.out.println("Delaer score: " + scoreDealer);
+            objPlayer._money = objPlayer._money - betAmount;
+            System.out.println("Dealer won!");
+            System.out.println("You lost $" + betAmount + "!");
+          } else if (scoreDealer > 21) {
+            System.out.println("Player score: " + score);
+            System.out.println("Delaer score: " + scoreDealer);
+            System.out.println("Dealer busted!");
+            System.out.println("Player won!");
+            System.out.println("You won $" + betAmount + "!");
+          } else if (score > 21) {
+            System.out.println("Player score: " + score);
+            System.out.println("Delaer score: " + scoreDealer);
+            System.out.println("Player busted!");
+            objPlayer._money = objPlayer._money - betAmount;
+            System.out.println("Dealer won!");
+            System.out.println("You lost $" + betAmount + "!");
+          }
+          endGame = false;
+          if (objPlayer._money == 0) {
+            System.out.println("You have no more money to bet!");
+            endGame = true;
+            objPlayer.writeInfo(name);
+            System.out.println("Thank you for playing Infinite Blackjack!");
+          }
+      } // loop - player plays a hand
       else if (choice.equalsIgnoreCase("N")) {
-        endGame = true;
-        // save player's information to a file
-        objPlayer.writeInfo(name);
-        System.out.println("Thank you for playing Infinite Blackjack!");
-      }
-    }// while endGame
-
-  }
+      endGame = true;
+      // save player's information to a file
+      objPlayer.writeInfo(name);
+      System.out.println("Thank you for playing Infinite Blackjack!");
+    }
+  } // while (endGame == false)
+}
 }
